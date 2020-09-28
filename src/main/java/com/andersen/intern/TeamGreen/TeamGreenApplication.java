096a8cba9392d4b5e7a715ff8e4fb0b88e7fad3e
Dmitry Falin <falin.d.14@mail.ru>
2020-09-28 21:59:05 +0300

    first commit

---
+++ src/main/java/com/andersen/intern/TeamGreen/TeamGreenApplication.java
@@ -0,0 +1,13 @@
+package com.andersen.intern.TeamGreen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamGreenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamGreenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andersen/intern/TeamGreen/TeamGreenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andersen.intern.TeamGreen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamGreenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
