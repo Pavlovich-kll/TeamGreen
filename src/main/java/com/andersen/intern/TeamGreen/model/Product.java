518f121c939c80bd60c42ef5709cf26b1772d5a0
Leonid Ryzhyk <leonx7@tut.by>
2020-10-03 11:19:41 +0300

    model: add entities Order, Product, Type

--- src/main/java/com/andersen/intern/TeamGreen/model/Product.java
+++ src/main/java/com/andersen/intern/TeamGreen/model/Product.java
@@ -1,2 +1,33 @@
-package com.andersen.intern.TeamGreen.model;public class Product {
+package com.andersen.intern.TeamGreen.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import java.util.Date;
+import java.util.Set;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+@Entity
+@Table(name = "products")
+public class Product extends AbstractBaseEntity{
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private long id;
+    private String description;
+    private long price;
+    @Lob
+    private byte image;
+    private Date created;
+    private Date lastUpdated;
+    @ManyToOne
+    @JoinColumn(name = "type_id")
+    private Type type;
+    @ManyToMany(mappedBy = "products")
+    Set<Order> orders;
 }
--- src/main/java/com/andersen/intern/TeamGreen/model/Type.java
+++ src/main/java/com/andersen/intern/TeamGreen/model/Type.java
@@ -1,2 +1,21 @@
-package com.andersen.intern.TeamGreen.model;public class Type {
+package com.andersen.intern.TeamGreen.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import java.util.Set;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+@Entity
+@Table(name = "types")
+public class Type extends AbstractNamedEntity{
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private long id;
+    private String name;
+    @OneToMany(mappedBy = "type")
+    private Set<Product> images;
 }
